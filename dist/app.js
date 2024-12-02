5c19f5f468d915d7835a132951c48b44f5485e12
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-12-02 19:43:24 +0200

    Changed DB initialiation logic

--- dist/app.js
+++ dist/app.js
@@ -7,7 +7,8 @@ const express_1 = __importDefault(require("express"));
 const path_1 = __importDefault(require("path"));
 const morgan_1 = __importDefault(require("morgan"));
 const routes_1 = __importDefault(require("./routes"));
-const database_1 = __importDefault(require("./config/database"));
+// import database from "./config/database"; 
+const mongoose_1 = __importDefault(require("mongoose"));
 // Create app
 const app = (0, express_1.default)();
 // Add middleware
@@ -17,5 +18,13 @@ app.use((0, morgan_1.default)("dev"));
 app.use('/', routes_1.default);
 // Serve static files
 app.use(express_1.default.static(path_1.default.join(__dirname, '../public')));
-database_1.default;
+const address = "mongodb://127.0.0.1:27017/testdb";
+mongoose_1.default.connect(address).then(() => {
+    console.log("Connected to database.");
+}).catch((error) => {
+    console.error("Could not connect to database", error);
+});
+mongoose_1.default.Promise = Promise;
+const database = mongoose_1.default.connection;
+database.on("error", console.error.bind(console, "Database connection error."));
 exports.default = app;
--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { Express, Request, Response } from "express";
 import path from "path"; 
 import morgan from "morgan";
 import router from "./routes"; 
-import database from "./config/database"
+// import database from "./config/database"; 
+import mongoose from 'mongoose'; 
 
 // Create app
 const app: Express = express()
@@ -17,6 +18,15 @@ app.use('/', router)
 // Serve static files
 app.use(express.static(path.join(__dirname, '../public')))
 
-database; 
+const address = "mongodb://127.0.0.1:27017/testdb"
+mongoose.connect(address).then(() => {
+  console.log("Connected to database.")
+}).catch((error) => {
+  console.error("Could not connect to database", error)
+}); 
+mongoose.Promise = Promise; 
+const database = mongoose.connection; 
+
+database.on("error", console.error.bind(console, "Database connection error."))
 
 export default app; 
