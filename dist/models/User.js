508960457c66663c72c849c29de6a47e3dad4bc6
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2024-12-02 19:36:13 +0200

    Changes to models

--- dist/models/User.js
+++ dist/models/User.js
@@ -2,14 +2,14 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Todo = exports.User = void 0;
 const mongoose_1 = require("mongoose");
-let TodoSchema = new mongoose_1.Schema({
-    todo: { type: String, required: true }
+const TodoSchema = new mongoose_1.Schema({
+    todo: { type: String, required: true },
 });
-const Todo = (0, mongoose_1.model)("TUser", TodoSchema);
-exports.Todo = Todo;
 const UserSchema = new mongoose_1.Schema({
     name: { type: String, required: true },
-    todos: [TodoSchema]
+    todos: [TodoSchema],
 });
+const Todo = (0, mongoose_1.model)("Todo", TodoSchema);
+exports.Todo = Todo;
 const User = (0, mongoose_1.model)("User", UserSchema);
 exports.User = User;
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,25 +1,24 @@
-import mongoose, { Document, Types, Schema, ObjectId, model } from "mongoose";
+import mongoose, { Document, Schema, model } from "mongoose";
 
-interface ITodo extends Document {
-  todo: string; 
+interface ITodo {
+  todo: string;
 }
 
-let TodoSchema = new Schema<ITodo>({
-  todo: { type: String, required: true }
-})
-
-const Todo = model<ITodo>("TUser", TodoSchema); 
-
-interface IUser extends Document {
-  name: string, 
-  todos: ITodo[]
-}
-
-const UserSchema = new Schema({
-  name: { type: String, required: true },
-  todos: [ TodoSchema ]
+const TodoSchema = new Schema<ITodo>({
+  todo: { type: String, required: true },
 });
 
+interface IUser extends Document {
+  name: string;
+  todos: ITodo[];
+}
+
+const UserSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  todos: [TodoSchema],
+});
+
+const Todo = model<ITodo>("Todo", TodoSchema);
 const User = model<IUser>("User", UserSchema);
 
 export { User, Todo };
